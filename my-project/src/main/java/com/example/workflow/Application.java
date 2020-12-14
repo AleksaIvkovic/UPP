a9ba5714d485aac9b8d0d84a72352820f7a14635
sarapanic <58145766+sarapanic@users.noreply.github.com>
2020-12-14 13:06:26 +0100

    Uspostavljena konekcija ka SQL bazi

--- my-project/src/main/java/com/example/workflow/Application.java
+++ my-project/src/main/java/com/example/workflow/Application.java
@@ -2,12 +2,54 @@ package com.example.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
+
     SpringApplication.run(Application.class);
+
+
+    String connectionUrl = "jdbc:sqlserver://localhost:1433;integratedSecurity=true;";
+
+    try {
+      System.out.print("Connecting to SQL Server ... ");
+      try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+        System.out.println("Done.");
+
+        System.out.print("Dropping and creating database 'SampleDB' ... ");
+        String sql = "DROP DATABASE IF EXISTS [SampleDB]; CREATE DATABASE [SampleDB]";
+        try (Statement statement = connection.createStatement()) {
+          statement.executeUpdate(sql);
+          System.out.println("Done.");
+        }
+
+        // Create a Table and insert some sample data
+        System.out.print("Creating sample table with data, press ENTER to continue...");
+        System.in.read();
+        sql = new StringBuilder().append("USE SampleDB; ").append("CREATE TABLE Employees ( ")
+                .append(" Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ").append(" Name NVARCHAR(50), ")
+                .append(" Location NVARCHAR(50) ").append("); ")
+                .append("INSERT INTO Employees (Name, Location) VALUES ").append("(N'Jared', N'Australia'), ")
+                .append("(N'Nikita', N'India'), ").append("(N'Tom', N'Germany'); ").toString();
+        try (Statement statement = connection.createStatement()) {
+          statement.executeUpdate(sql);
+          System.out.println("Done.");
+        }
+
+
+      }
+    } catch (Exception e) {
+      System.out.println();
+      e.printStackTrace();
+    }
+
+
+
   }
 
 }
