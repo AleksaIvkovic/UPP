8ebcaa8a775096c5de17affdfa29100bcf7e67b9
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2020-12-15 13:18:00 +0100

    Podesena konfiguracija aplikacije

---
+++ my-project/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO genre(name) VALUES ('Drama');
+INSERT INTO genre(name) VALUES ('Romance');
+INSERT INTO genre(name) VALUES ('Kids');
+INSERT INTO genre(name) VALUES ('Horror');
+INSERT INTO genre(name) VALUES ('History');
