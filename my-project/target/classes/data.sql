e55b10cf3b1f03e6db0b765a46214a0ce5e6a7f6
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2020-12-18 21:04:29 +0100

    Dodati korisnici u sql skriptu

--- my-project/target/classes/data.sql
+++ my-project/target/classes/data.sql
@@ -1,6 +1,18 @@
-INSERT INTO genre(name) VALUES ('Comedy');
-INSERT INTO genre(name) VALUES ('Drama');
-INSERT INTO genre(name) VALUES ('Romance');
-INSERT INTO genre(name) VALUES ('Kids');
-INSERT INTO genre(name) VALUES ('Horror');
-INSERT INTO genre(name) VALUES ('History');
+--INSERT INTO genre(name) VALUES ('Comedy');
+--INSERT INTO genre(name) VALUES ('Drama');
+--INSERT INTO genre(name) VALUES ('Romance');
+--INSERT INTO genre(name) VALUES ('Kids');
+--INSERT INTO genre(name) VALUES ('Horror');
+--INSERT INTO genre(name) VALUES ('History');
+
+USE [uppDB]
+GO
+
+INSERT INTO [dbo].[genre] ([name]) VALUES ('Comedy')
+INSERT INTO [dbo].[genre] ([name]) VALUES ('Drama')
+INSERT INTO [dbo].[genre] ([name]) VALUES ('Romance')
+INSERT INTO [dbo].[genre] ([name]) VALUES ('Kids')
+INSERT INTO [dbo].[genre] ([name]) VALUES ('Horror')
+INSERT INTO [dbo].[genre] ([name]) VALUES ('History')
+INSERT INTO [dbo].[genre] ([name]) VALUES ('Psychology')
+GO
